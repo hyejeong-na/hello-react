525298711c53b0bc138a9bc9fceff99016834f45
nhj1987 <nhj1987@cj.net>
2022-11-07 23:07:53 +0900

    second22

--- src/Average.js
+++ src/Average.js
@@ -1,7 +1,7 @@
 import {useState,useMemo,useRef} from 'react';
 
 const getAverage = numbers =>{
-    console.log('평균값 계산 중 ......');
+    console.log('평균값 계산 중? ......');
     if(numbers.length === 0) return 0;
     const sum = numbers.reduce((a,b) => a+b);
     return sum/ numbers.length;
